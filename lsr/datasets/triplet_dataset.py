f3be1421fd49ae06cf068ed7c04dff6282539678
Thong Nguyen <thongnguyenthac@gmail.com>
2023-01-24 17:07:30 +0100

    can load datasets from ir_datasets

--- lsr/datasets/triplet_dataset.py
+++ lsr/datasets/triplet_dataset.py
@@ -2,26 +2,21 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 import torch
 
+from lsr.utils.dataset_utils import read_collection, read_queries
+
 
 class TripletIdsDataset(Dataset):
     """BM25 triplets of (query_id, pos_id, neg_id)"""
 
-    def __init__(self, triple_ids_path: str, queries_path: str, docs_path: str) -> None:
+    def __init__(
+        self, triple_ids_path: str, queries_path: str, collection_path: str
+    ) -> None:
         super().__init__()
         self.triple_lists = []
         docs_dict = {}
         queries_dict = {}
-        with open(docs_path) as f:
-            for line in tqdm(f, desc=f"Loading docs collection from {docs_path}"):
-                try:
-                    docid, doctext = line.strip().split("\t")
-                    docs_dict[docid] = doctext
-                except:
-                    pass
-        with open(queries_path) as f:
-            for line in tqdm(f, desc=f"Loading queries from {queries_path}"):
-                qid, qtext = line.strip().split("\t")
-                queries_dict[qid] = qtext
+        docs_dict = read_collection(collection_path)
+        queries_dict = dict(read_queries(queries_path))
         with open(triple_ids_path) as f:
             for line in tqdm(f, desc=f"Loading id triples from {triple_ids_path}"):
                 qid, pos_id, neg_id = line.strip().split("\t")

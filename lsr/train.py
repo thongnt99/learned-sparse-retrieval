6d36c9661262e7e295cb2fe986c909b159a619a3
Thong Nguyen <thongnguyenthac@gmail.com>
2024-02-23 16:28:34 +0100

    - Creating output directory from the name of config file - Add date time

--- lsr/train.py
+++ lsr/train.py
@@ -1,17 +1,28 @@
 from pprint import pprint
 import hydra
 from hydra.utils import instantiate
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig, OmegaConf, open_dict
 from pprint import pprint
 import logging
 import wandb
 import os
+import datetime
+from hydra.core.hydra_config import HydraConfig
 
 logger = logging.getLogger(__name__)
 
 
 @hydra.main(version_base="1.2", config_path="configs", config_name="config")
 def train(conf: DictConfig):
+    hydra_cfg = HydraConfig.get()
+    experiment_name = hydra_cfg.runtime.choices["experiment"]
+    now = datetime.now()
+    run_name = f"{experiment_name}/{now.strftime('%m-%d-%Y')}"
+    output_dir = f"./outputs/{run_name}"
+    with open_dict(conf):
+        conf.training_arguments.output_dir = output_dir
+        conf.training_arguments.run_name = run_name
+    resolved_conf = OmegaConf.to_container(conf, resolve=True)
     resolved_conf = OmegaConf.to_container(conf, resolve=True)
     pprint(resolved_conf)
     os.environ["WANDB_PROJECT"] = conf.wandb.setup.project

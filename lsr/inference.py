d702026aacf1ab7c47011f55edcb2646a6bb646d
Thong Nguyen <thongnguyenthac@gmail.com>
2024-02-29 12:23:40 +0100

    raw float weights

--- lsr/inference.py
+++ lsr/inference.py
@@ -19,12 +19,12 @@ logger = logging.getLogger(__name__)
 
 
 def write_to_file(f, result, type):
-    if type == "query":
-        rep_text = " ".join(Counter(result["vector"]).elements()).strip()
-        if len(rep_text) > 0:
-            f.write(f"{result['id']}\t{rep_text}\n")
-    else:
-        f.write(json.dumps(result) + "\n")
+    # if type == "query":
+    #     rep_text = " ".join(Counter(result["vector"]).elements()).strip()
+    #     if len(rep_text) > 0:
+    #         f.write(f"{result['id']}\t{rep_text}\n")
+    # else:
+    f.write(json.dumps(result) + "\n")
 
 
 @hydra.main(version_base="1.2", config_path="configs", config_name="config")
@@ -42,17 +42,17 @@ def inference(cfg: DictConfig,):
     output_path = Path(cfg.output_dir).joinpath(cfg.output_file)
     file_writer = open(output_path, "w")
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    logger.log(msg=f"Running inference on {device}",level=1)
-    logger.log(msg=f"Loading model from {cfg.model_path}",level=1)
+    logger.log(msg=f"Running inference on {device}", level=1)
+    logger.log(msg=f"Loading model from {cfg.model_path}", level=1)
     model = DualSparseEncoder.from_pretrained(cfg.model_path)
     model.eval()
     model.to(device)
     tokenizer_path = os.path.join(cfg.model_path, "tokenizer")
-    logger.log(msg=f"Loading tokenizer from {tokenizer_path}",level=1)
+    logger.log(msg=f"Loading tokenizer from {tokenizer_path}", level=1)
     tokenizer = Tokenizer.from_pretrained(tokenizer_path)
     ids = []
     texts = []
-    if cfg.input_format in ("tsv","json"):
+    if cfg.input_format in ("tsv", "json"):
         with open(cfg.input_path, "r") as f:
             if cfg.input_format == "tsv":
                 for line in tqdm(f, desc=f"Reading data from {cfg.input_path}"):
@@ -93,9 +93,9 @@ def inference(cfg: DictConfig,):
     all_token_ids = list(range(tokenizer.get_vocab_size()))
     all_tokens = np.array(tokenizer.convert_ids_to_tokens(all_token_ids))
     for idx in tqdm(range(0, len(ids), cfg.batch_size)):
-        logger.log(msg={"batch": idx},level=1)
-        batch_texts = texts[idx : idx + cfg.batch_size]
-        batch_ids = ids[idx : idx + cfg.batch_size]
+        logger.log(msg={"batch": idx}, level=1)
+        batch_texts = texts[idx: idx + cfg.batch_size]
+        batch_ids = ids[idx: idx + cfg.batch_size]
         batch_tkn = tokenizer(
             batch_texts,
             padding=True,
@@ -120,7 +120,8 @@ def inference(cfg: DictConfig,):
         if cfg.top_k > 0:
             # do top_k selection in batch
             top_k_res = batch_output.topk(dim=1, k=cfg.top_k, sorted=False)
-            batch_values = (top_k_res.values * cfg.scale_factor).to(torch.int)
+            batch_values = top_k_res.values
+            # (top_k_res.values * cfg.scale_factor).to(torch.int)
             indices = top_k_res.indices
             batch_tokens = all_tokens[indices]
             for text_id, text, tokens, weights in zip(
@@ -140,7 +141,7 @@ def inference(cfg: DictConfig,):
                 )
         else:
             # do non-zero selection
-            batch_output = (batch_output * cfg.scale_factor).to(torch.int)
+            # batch_output = (batch_output * cfg.scale_factor).to(torch.int)
             batch_tokens = [[] for _ in range(len(batch_ids))]
             batch_weights = [[] for _ in range(len(batch_ids))]
             for row_col in batch_output.nonzero():
@@ -152,7 +153,8 @@ def inference(cfg: DictConfig,):
             ):
                 write_to_file(
                     file_writer,
-                    {"id": text_id, "text": text, "vector": dict(zip(tokens, weights))},
+                    {"id": text_id, "text": text,
+                        "vector": dict(zip(tokens, weights))},
                     cfg.type,
                 )
 
@@ -161,4 +163,5 @@ if __name__ == "__main__":
     start_time = time.time()
     inference()
     run_time = time.time() - start_time
-    logger.log(msg=f"Finished! Runing time {str(datetime.timedelta(seconds=666))}",level=1)
+    logger.log(
+        msg=f"Finished! Runing time {str(datetime.timedelta(seconds=666))}", level=1)

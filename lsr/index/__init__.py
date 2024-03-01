63191746e61b4a9742562754c35045fc16e8c34b
Thong Nguyen <thongnguyenthac@gmail.com>
2024-03-01 14:36:06 +0100

    add anserini index

--- lsr/index/__init__.py
+++ lsr/index/__init__.py
@@ -1,9 +1,1 @@
-class Index:
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def index(self, doc_path):
-        pass
-
-    def retrieve(self, query_path):
-        pass
+from anserini import AnseriniIndex

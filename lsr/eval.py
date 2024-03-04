2b2d1e2a7733c5b47835a189a560065890bf74ce
Thong Nguyen <thongnguyenthac@gmail.com>
2024-03-05 00:44:39 +0100

    load from the final model

--- lsr/eval.py
+++ lsr/eval.py
@@ -16,6 +16,7 @@ def eval(conf: DictConfig):
     experiment_name = hydra_cfg.runtime.choices["experiment"]
     run_name = f"{experiment_name}"
     output_dir = f"./outputs/{run_name}"
+    model_dir = f"{output_dir}/model"
     with open_dict(conf):
         conf.training_arguments.output_dir = output_dir
         conf.training_arguments.run_name = run_name
@@ -33,7 +34,7 @@ def eval(conf: DictConfig):
     logger.info(f"Working directiory: {os.getcwd()}")
     trainer = instantiate(conf.trainer)
     test_dataset = instantiate(conf.test_dataset)
-    trainer._load_from_checkpoint(conf.resume_from_checkpoint)
+    trainer._load_from_checkpoint(model_dir)
     trainer.predict(test_dataset)
     wandb.finish()
 
